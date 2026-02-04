a67edb9a695d79a51c7dbebb6f3ef8cbb05fd802
Argovesta <anapanv@isise.edu.pe>
2026-02-03 19:15:07 -0500

    se añadieron indices, y la espacificacion esqueletica de los paquetes y cuerpos

---
+++ Estructura/CuerpoPaquetes.sql
@@ -0,0 +1,55 @@
+
+CREATE OR REPLACE PACKAGE BODY pkg_convocatorias
+AS
+
+END pkg_convocatorias;
+
+CREATE OR REPLACE PACKAGE BODY pkg_documentos
+AS
+
+END pkg_documentos;
+
+CREATE OR REPLACE PACKAGE BODY pkg_empresas
+AS
+
+END pkg_empresas;
+
+CREATE OR REPLACE PACKAGE BODY pkg_estados_postulacion
+AS
+
+END pkg_estados_postulacion;
+
+CREATE OR REPLACE PACKAGE BODY pkg_postulaciones 
+AS
+
+END pkg_postulaciones;
+
+CREATE OR REPLACE PACKAGE BODY pkg_resoluciones_postulacion
+AS
+
+END pkg_resoluciones_postulacion;
+
+CREATE OR REPLACE PACKAGE BODY pkg_roles_usuario
+AS
+
+END pkg_roles_usuario;
+
+CREATE OR REPLACE PACKAGE BODY pkg_tipos_documento
+AS
+
+END pkg_tipos_documento;
+
+CREATE OR REPLACE PACKAGE BODY pkg_tipos_jornada
+AS
+
+END pkg_tipos_jornada;
+
+CREATE OR REPLACE PACKAGE BODY pkg_tipos_modalidad 
+AS
+
+END pkg_tipos_modalidad;
+
+CREATE OR REPLACE PACKAGE BODY pkg_usuarios
+AS
+
+END pkg_usuarios;
---
+++ Estructura/EspacificacionPaquetes.sql
@@ -0,0 +1,57 @@
+-- establecer paquetes 
+
+CREATE OR REPLACE PACKAGE pkg_convocatorias
+AS
+
+END pkg_convocatorias;
+
+
+CREATE OR REPLACE PACKAGE pkg_documentos
+AS
+
+END pkg_documentos;
+
+CREATE OR REPLACE PACKAGE pkg_empresas
+AS
+
+END pkg_empresas;
+
+CREATE OR REPLACE PACKAGE pkg_estados_postulacion
+AS
+
+END pkg_estados_postulacion;
+
+CREATE OR REPLACE PACKAGE pkg_postulaciones
+AS
+
+END pkg_postulaciones;
+
+CREATE OR REPLACE PACKAGE pkg_resoluciones_postulacion
+AS
+
+END pkg_resoluciones_postulacion;
+
+CREATE OR REPLACE PACKAGE pkg_roles_usuario
+AS
+
+END pkg_roles_usuario;
+
+CREATE OR REPLACE PACKAGE pkg_tipos_documento
+AS
+
+END pkg_tipos_documento;
+
+CREATE OR REPLACE PACKAGE pkg_tipos_jornada
+AS
+
+END pkg_tipos_jornada;
+
+CREATE OR REPLACE PACKAGE pkg_tipos_modalidad
+AS
+
+END pkg_tipos_modalidad;
+
+CREATE OR REPLACE PACKAGE pkg_usuarios
+AS
+
+END pkg_usuarios;
